93658da91f0cd9e2a9ef7872c0181fc68c4b57b4
DAMIANCANO1 <canodamian121@gmail.com>
2026-03-05 13:29:11 -0600

    progra2

--- src/repaso/SelectionSort.java
+++ src/repaso/SelectionSort.java
@@ -36,77 +36,130 @@ public class SelectionSort {
             System.out.println("ingresa el elemento " + i + " para el arreglo: ");
             Desordenado[i] = Enter.nextInt();
             System.out.println();
-
         }
+
         System.out.println();
         System.out.println("EL ARREGLO A SER ORDENADO POR EL METODO DE SELECCION ES : ");
         for (int i = 0; i < Desordenado.length; i++) {
             System.out.println("|" + Desordenado[i] + "|");
 
         }
-        System.out.println();
-        
-        //se decalra un arreglo temporal que ira
-        //conteniendo a los subarreglos del arreglo org
+        boolean Bandera = false;
+        for (int e = 0; Bandera = true; e++) {
+            System.out.println("INGRESA 1 PARA ORDENAR DE MENOR A MAYOR");
+            System.out.println("INGRESA 2 PARA ORDENAR DE MAYOR A MENOR");
+            System.out.println("INGRESA 3 PARA SALIR");
+            int Menu = Enter.nextInt();
+            if (Menu == 1) {
+                System.out.println();
 
-        int temp[];
-        //se declara una variable de apoyo que manejara 
-        // la longitud cambiante del arreglo temporal
-        // de manera que solo se almacenen elementos requeridos
+                //se declara un arreglo temporal que ira
+                //conteniendo a los subarreglos del arreglo org
+                int temp[];
+                //se declara una variable de apoyo que manejara 
+                // la longitud cambiante del arreglo temporal
+                // de manera que solo se almacenen elementos requeridos
 
-        int leng = Desordenado.length;
-        //se decalra una varianle que ayudara para
-        //copiar los elemtos necesarios del arreglo 
-        //original al temporal
-        int t;
-        //se declarfa un ciclo principal sobre
-        //el que se recorre todo el arreglo principal
-        //y permitira ir creando sub arreglos
-        for (int i =0; i < Desordenado.length; i++) {
-            //igualar t a i para que el sub arreglo
-            //pueda tener los elementos del arreglo
-            //original conforme se vayan necesitando
-            //se comienza con el arreglo completo y 
-            //se ira quitando una posicion en cada vuelta
-            t = i;
-            //se inicializa el arreglo temporal con la 
-            //cantidad de posiciones que solo se van a 
-            //necesitar. La variable lengh ira cambiando
-            //conforme se vayan quitando posiciones del 
-            //arreglo original
-            temp = new int[leng];
-            //se procede a llenar el arreglo temporar con los
-            //elementos requeridos del arreglo original
-            for (int k = 0; k < temp.length; k++) {
-                //se va copiando al arreglo temporar los
-                //elemento que se necesitan
-                temp[k] = Desordenado[t];
-                t++;
+                int leng = Desordenado.length;
+                //se declara una varianle que ayudara para
+                //copiar los elemtos necesarios del arreglo 
+                //original al temporal
+                int t;
+                //se declarfa un ciclo principal sobre
+                //el que se recorre todo el arreglo principal
+                //y permitira ir creando sub arreglos
+                for (int i = 0; i < Desordenado.length; i++) {
+                    //igualar t a i para que el sub arreglo
+                    //pueda tener los elementos del arreglo
+                    //original conforme se vayan necesitando
+                    //se comienza con el arreglo completo y 
+                    //se ira quitando una posicion en cada vuelta
+                    t = i;
+                    //se inicializa el arreglo temporal con la 
+                    //cantidad de posiciones que solo se van a 
+                    //necesitar. La variable lengh ira cambiando
+                    //conforme se vayan quitando posiciones del 
+                    //arreglo original
+                    temp = new int[leng];
+                    //se procede a llenar el arreglo temporar con los
+                    //elementos requeridos del arreglo original
+                    for (int k = 0; k < temp.length; k++) {
+                        //se va copiando al arreglo temporar los
+                        //elemento que se necesitan
+                        temp[k] = Desordenado[t];
+                        t++;
+                    }
+                    //se localiza el elemento minimo del arreglo actual
+                    Minimo = encuentraMinimo(temp);
+                    index = encuentraposicion(Desordenado, Minimo);
+
+                    //almacenar en una variable temporal el valor de 
+                    //la posicion actual en la que esta en el arreglo
+                    Tempi = Desordenado[i];
+
+                    Desordenado[i] = Desordenado[index];
+                    Desordenado[index] = Tempi;
+                    //se disminuye la variable leng para ir disminuyendp
+                    //las posiciones a ordenar del del arreglo principal
+                    leng--;
+                }
+                System.out.println("El arreglo ordenado es:");
+                System.out.println();
+                for (int i = 0; i < Desordenado.length; i++) {
+                    System.out.println("|" + Desordenado[i] + "|");
+
+                }
+
+            } else if (Menu == 2) {
+                int temp[];
+              
+                int leng = Desordenado.length;
+
+                int t;
+
+                for (int i = Desordenado.length-1; i >=0; i++) {
+                    //igualar t a i para que el sub arreglo
+                    //pueda tener los elementos del arreglo
+                    //original conforme se vayan necesitando
+                    //se comienza con el arreglo completo y 
+                    //se ira quitando una posicion en cada vuelta
+                    t = i;
+                    //se inicializa el arreglo temporal con la 
+                    //cantidad de posiciones que solo se van a 
+                    //necesitar. La variable lengh ira cambiando
+                    //conforme se vayan quitando posiciones del 
+                    //arreglo original
+                    temp = new int[leng];
+                    //se procede a llenar el arreglo temporar con los
+                    //elementos requeridos del arreglo original
+                    for (int k = 0; k < temp.length; k++) {
+                        //se va copiando al arreglo temporar los
+                        //elemento que se necesitan
+                        temp[k] = Desordenado[t];
+                        t++;
+                    }
+                    //se localiza el elemento minimo del arreglo actual
+                    Minimo = encuentraMinimo(temp);
+                    index = encuentraposicion(Desordenado, Minimo);
+
+                    //almacenar en una variable temporal el valor de 
+                    //la posicion actual en la que esta en el arreglo
+                    Tempi = Desordenado[i];
+
+                    Desordenado[i] = Desordenado[index];
+                    Desordenado[index] = Tempi;
+                    //se disminuye la variable leng para ir disminuyendp
+                    //las posiciones a ordenar del del arreglo principal
+                    leng--;
+
+                }
+            } else if (Menu == 3) {
+                Bandera = true;
+                break;
             }
-            //se localiza el elemento minimo del arreglo actual
-            Minimo = encuentraMinimo(temp);
-            index = encuentraposicion(Desordenado, Minimo);
-            
-            //almacenar en una variable temporal el valor de 
-            //la posicion actual en la que esta en el arreglo
-            
-            Tempi = Desordenado[i];
-            
-            Desordenado[i]=Desordenado[index];
-            Desordenado[index]= Tempi;
-            //se disminuye la variable leng para ir disminuyendp
-            //las posiciones a ordenar del del arreglo principal
-            leng--;
         }
-        System.out.println("El arreglo ordenado es:");
-        System.out.println();
-        for (int i = 0; i < Desordenado.length; i++) {
-            System.out.println("|"+Desordenado[i]+"|");
-            
-        }
-        
-        
     }
+
     public static int encuentraMinimo(int arr[]) {
         int min = arr[0];
         int mayor = arr[0];
@@ -121,15 +174,18 @@ public class SelectionSort {
         }
         return min;
     }
-    
-    public static int encuentraposicion (int ar[], int v){
+
+    public static int encuentraposicion(int ar[], int v) {
         for (int i = 0; i < ar.length; i++) {
-            if (v == ar [i]){
+            if (v == ar[i]) {
                 return i;
             }
-            
+
         }
         return 0;
     }
-
 }
+
+    
+
+
