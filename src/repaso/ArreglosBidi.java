c843fde1a29dd3974a732495dd0dd6e3cba745ab
canod <canod@LAPTOP-9FR7JI32.home>
2026-03-24 12:33:42 -0600

    Repaso

--- src/repaso/ArreglosBidi.java
+++ src/repaso/ArreglosBidi.java
@@ -16,13 +16,18 @@ public class ArreglosBidi {
         int Filas = Integer.parseInt(JOptionPane.showInputDialog(null, "INGRESA LAS FILAS DEL ARRAY"));
         int Columnas = Integer.parseInt(JOptionPane.showInputDialog(null, "INGRESA LAS COLUMNAS DEL ARRAY"));
         int arr[][] = new int[Filas][Columnas];
-
+        
+        JOptionPane.showMessageDialog(null, "ARREGLO BIDIMENSIONAL BASICO");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 arr[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null, "INGRESA VALOR"));
             }
         }
-        
+        ImprimeArray(arr);
+        JOptionPane.showMessageDialog(null, "Tableros con arreglos bidimensionales");
+    }
+    
+    public static void ImprimeArray(int arr[][]) {
         System.out.println("EL ARREGLO BIDIMENSIONAL ES:  \n");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
@@ -30,7 +35,18 @@ public class ArreglosBidi {
             }
             System.out.println("");
         }
-
+    }
+    
+    public static void DiagonalEnCero(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (i == j) {
+                    arr[i][j] = 0;
+                } else {
+                    arr[i][j] = arr[i][j];
+                }
+            }
+        }
     }
 
 }
