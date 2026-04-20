e61e9d96656b7b11c011a68c32e589d6722f7366
canod <canod@LAPTOP-9FR7JI32.home>
2026-04-20 17:45:25 -0600

    MATRICES

--- src/repaso/OperacionesMatrices.java
+++ src/repaso/OperacionesMatrices.java
@@ -11,8 +11,6 @@ package repaso;
 public class OperacionesMatrices {
 
     public static void main(String args[]) {
-        //se decalra un arr final para los resultados 
-        int Resultados[] = new int[3];
         //se declaran las matrices a operar 
         int a[][] = new int[3][3];
         int b[][] = new int[3][3];
@@ -28,25 +26,65 @@ public class OperacionesMatrices {
         ImprimeArray(a);
         System.out.println();
         ImprimeArray(b);
-
-        for (int i = 0; i < a[0].length; i++) {
-            for (int j = 0; j < a[0].length; j++) {
-                x = j;
-                y = i;
-                suma += a[i][j] * b[x][y];
-
-            }
-            Resultados[i] = suma;
-            suma = 0;
-
-        }
-
         System.out.println();
 
-        for (int i = 0; i < Resultados.length; i++) {
-            System.out.print(Resultados[i] + " ");
+        //Resultados multiplicacion
+        System.out.println("MULTIPLICACION");
+        int Resultado[][] = multiplicarMatrices(a, b);
+        ImprimeArray(Resultado);
+        
+        //Resultados suma
+        System.out.println("SUMA");
+        Resultado = sumarMatrices(a, b);
+        ImprimeArray(Resultado);
+        
+        //Resultados resta
+        System.out.println("RESTA");
+        Resultado = restarMatrices(a, b);
+        ImprimeArray(Resultado);
 
+    }
+
+    public static int[][] multiplicarMatrices(int a[][], int b[][]) {
+        int Resultados[][] = new int[3][3];
+        for (int i = 0; i < a.length; i++) {              // FILA del resultado
+            for (int j = 0; j < b[0].length; j++) {       // COLUMNA del resultado
+                int suma = 0;
+                for (int k = 0; k < a[0].length; k++) {    // MULTIPLICACIÓN
+                    suma += a[i][k] * b[k][j];
+                }
+                Resultados[i][j] = suma;
+            }
         }
+        return Resultados;
+    }
+
+    public static int[][] sumarMatrices(int a[][], int b[][]) {
+        int Resultados[][] = new int[3][3];
+
+        for (int i = 0; i < a.length; i++) {           // FILAS
+            for (int j = 0; j < a[0].length; j++) {    // COLUMNAS
+                int Suma = a[i][j] + b[i][j];
+                Resultados[i][j] = Suma;
+                Suma = 0;
+            }
+        }
+
+        return Resultados;
+    }
+    
+    public static int[][] restarMatrices(int a[][], int b[][]) {
+        int Resultados[][] = new int[3][3];
+
+        for (int i = 0; i < a.length; i++) {           // FILAS
+            for (int j = 0; j < a[0].length; j++) {    // COLUMNAS
+                int Suma = a[i][j] - b[i][j];
+                Resultados[i][j] = Suma;
+                Suma = 0;
+            }
+        }
+
+        return Resultados;
 
     }
 
